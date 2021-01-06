64b242c9f9b1abe040af7c23511a1f459a425402
Abolfazl Ziaeemehr <a.ziaeemehr@gmail.com>
2021-01-06 17:47:15 +0330

    gpe two cell added. dimensions of parameters modified.

--- terman2002/Brian/gpe_cell/lib.py
+++ terman2002/Brian/gpe_cell/lib.py
@@ -3,10 +3,85 @@ import pylab as plt
 import brian2 as b2
 
 
+def simulate_two_GPe_cell(par_g, par_sim):
+
+    b2.defaultclock.dt = par_sim['dt']
+
+    eqs = '''
+    i_extg : amp
+
+    ainfg = 1 / (1 + exp(-(vg - thetaag*mV) / (sigag*mV))) : 1
+    sinfg = 1 / (1 + exp(-(vg - thetasg*mV) / (sigsg*mV))) : 1
+    rinfg = 1 / (1 + exp(-(vg - thetarg*mV) / (sigrg*mV))) : 1
+    minfg = 1 / (1 + exp(-(vg - thetamg*mV) / (sigmg*mV))) : 1
+    ninfg = 1 / (1 + exp(-(vg - thetang*mV) / (signg*mV))) : 1
+    hinfg = 1 / (1 + exp(-(vg - thetahg*mV) / (sighg*mV))) : 1
+    taung = taun0g + taun1g / (1 + exp(-(vg - thngt*mV) / (sng*mV))) : second
+    tauhg = tauh0g + tauh1g / (1 + exp(-(vg - thhgt*mV) / (shg*mV))) : second
+
+    itg = gtg * (ainfg ** 3) * rg * (vg - vcag) : amp
+    inag = gnag * (minfg ** 3) * hg * (vg - vnag) : amp
+    ikg = gkg * (ng ** 4) * (vg - vkg) : amp
+    iahpg = gahpg * (vg - vkg) * cag / (cag + k1g) : amp
+    icag = gcag * (sinfg ** 2) * (vg - vcag) : amp
+    ilg = glg * (vg - vlg) : amp
+    i_syn_gg : amp
+
+    Hinf = 1./(1+exp(-(vg - thetag * mV - thetagH*mV) / (sigmagH*mV))):1
+    ds_gg/dt = alpha * Hinf * (1 - s_gg) - beta * s_gg  : 1
+
+    membrane_Im =  -(itg + inag + ikg + iahpg + icag + ilg) + i_extg + i_syn_gg : amp
+    dhg/dt = phihg*(hinfg-hg)/tauhg : 1
+    dng/dt = phing*(ninfg-ng)/taung : 1
+    drg/dt = phig*(rinfg-rg)/taurg  : 1
+    dcag/dt= epsg*((-icag-itg)/pA - kcag*cag) : 1  #! pA
+
+    dvg/dt = membrane_Im / C : volt
+    '''
+
+    neurons = b2.NeuronGroup(2,
+                             eqs,
+                             method=par_sim['integration_method'],
+                             dt=par_sim['dt'],
+                             threshold='vg>-20*mV',
+                             refractory='vg>-20*mV',
+                             namespace=par_g,
+                             )
+    eqs_syn_gg = '''  # synapse model, g to g
+    i_syn_gg_post = w_gg * s_gg_pre * (v_rev_gg - vg) :amp (summed)
+    '''
+
+    syn_gg = b2.Synapses(neurons, neurons,
+                         eqs_syn_gg,
+                         dt=par_sim['dt'],
+                         method=par_sim['integration_method'],
+                         namespace=par_g,
+                         )
+    syn_gg.connect(i=0, j=1)
+
+    neurons.vg = par_g['v0']
+    neurons.hg = "hinfg"
+    neurons.ng = "ninfg"
+    neurons.rg = "rinfg"
+    neurons.cag = 0
+    neurons.i_extg = par_g['iapp']
+
+    st_mon = b2.StateMonitor(neurons, ["vg", 'i_syn_gg', 's_gg'],
+                             record=True)
+
+    net = b2.Network(neurons)
+    net.add(syn_gg)
+    net.add(st_mon)
+    net.run(par_sim['simulation_time'])
+
+    return st_mon
+
+
 def simulate_GPe_cell(par_g, par_sim):
 
     num = par_g['num']
     input_current = par_g['i_ext']
+    b2.defaultclock.dt = par_sim['dt']
 
     eqs = '''
     i_extg = input_current(t, i): amp
@@ -31,7 +106,7 @@ def simulate_GPe_cell(par_g, par_sim):
     dhg/dt = phihg*(hinfg-hg)/tauhg : 1
     dng/dt = phing*(ninfg-ng)/taung : 1
     drg/dt = phig*(rinfg-rg)/taurg  : 1
-    dcag/dt= epsg*((-icag-itg)/uamp - kcag*cag) : 1
+    dcag/dt= epsg*((-icag-itg)/pA - kcag*cag) : 1  #! pA 
 
     dvg/dt = membrane_Im / C : volt
     '''
@@ -61,17 +136,19 @@ def simulate_GPe_cell(par_g, par_sim):
     return st_mon
 
 
-def plot_data(st_mon, ax):
+def plot_data(st_mon, ax, title=None):
     ax.plot(st_mon.t / b2.ms, st_mon.vg[0] / b2.mV, lw=2, c='k')
     ax.set_xlim(0, np.max(st_mon.t / b2.ms))
     # ax.set_xlabel("time [ms]", fontsize=14)
     ax.set_ylabel("v [mV]", fontsize=14)
+    if title is not None:
+        ax.set_title("I = {:s}".format(title))
 
 
-def plot_current(st_mon, ax, ylim=None):
+def plot_current(st_mon, current_unit=b2.pA, ax=None, ylim=None):
     ax.plot(st_mon.t / b2.ms,
-            st_mon.i_extg[0] / b2.uamp, lw=1, c='k', alpha=0.5)
+            st_mon.i_extg[0] / current_unit, lw=1, c='k', alpha=0.5)
     ax.set_xlabel("t [ms]", fontsize=14)
-    ax.set_ylabel("I [micro A]", fontsize=14)
+    ax.set_ylabel("I [{}]".format(str(current_unit)), fontsize=14)
     if ylim is not None:
         ax.set_ylim(ylim)
--- terman2002/Brian/gpe_cell/main.py
+++ terman2002/Brian/gpe_cell/main.py
@@ -3,28 +3,29 @@ import brian2 as b2
 import pylab as plt
 from time import time
 from lib import (simulate_GPe_cell,
+                 simulate_two_GPe_cell,
                  plot_data,
                  plot_current)
-from input_factory import get_step_current                 
+from input_factory import get_step_current
 
 if __name__ == "__main__":
 
     par_g = {
         'num': 1,
         'v0': -55 * b2.mV,
-        'vnag': 55.* b2.mV,
-        'vkg': -80.* b2.mV,
-        'vcag': 120.* b2.mV,
-        'vlg': -55.* b2.mV,
-        'gnag': 120. * b2.msiemens,
-        'gkg': 30.* b2.msiemens,
-        'gahpg': 30.* b2.msiemens,
-        'gtg': .5* b2.msiemens,
-        'gcag': .1* b2.msiemens,
-        'glg': .1* b2.msiemens,
+        'vnag': 55. * b2.mV,
+        'vkg': -80. * b2.mV,
+        'vcag': 120. * b2.mV,
+        'vlg': -55. * b2.mV,
+        'gnag': 120. * b2.nS,
+        'gkg': 30. * b2.nS,
+        'gahpg': 30. * b2.nS,
+        'gtg': .5 * b2.nS,
+        'gcag': .1 * b2.nS,
+        'glg': .1 * b2.nS,
 
-        'taurg': 30. *b2.ms,
-        'taun0g': .05 *b2.ms,
+        'taurg': 30. * b2.ms,
+        'taun0g': .05 * b2.ms,
         'taun1g': .27*b2.ms,
         'tauh0g': .05*b2.ms,
         'tauh1g': .27*b2.ms,
@@ -46,15 +47,22 @@ if __name__ == "__main__":
         'sng': -12,
         'shg': -12,
         'k1g': 30.,
-        'kcag': 20., #Report:15,  Terman Rubin 2002: 20.0
+        'kcag': 20.,  # Report:15,  Terman Rubin 2002: 20.0
         'phig': 1.,
-        'phing': .05, # Report: 0.1, Terman Rubin 2002: 0.05
+        'phing': .05,  # Report: 0.1, Terman Rubin 2002: 0.05
         'phihg': .05,
         'alphag': 2,
         'betag': .08,
         'epsg': 0.0001 / b2.ms,
         # 'iapp': 0.0 * b2.uamp,
-        'C': 1 * b2.ufarad,
+        'C': 1 * b2.pF,
+        'w_gg': 1.0*b2.nS,
+        'thetag': 20.,
+        'thetagH': -57.,
+        'sigmagH': 2.,
+        'beta': 0.08 /b2.ms,
+        'alpha': 2/b2.ms,
+        "v_rev_gg":-100.*b2.mV
     }
 
     par_sim = {
@@ -63,22 +71,66 @@ if __name__ == "__main__":
         'dt': 0.01 * b2.ms,
     }
 
-    start_time = time()
-    # Figure 2 ------------------------------------------------------
-    fig, ax = plt.subplots(4, figsize=(10, 6), sharex=True)
-    i_stim = [20, 0, -0.5]
-    for ii in range(len(i_stim)):
-        input_current = get_step_current(0,
-                                         1000,
-                                         b2.ms,
-                                         i_stim[ii] * b2.uamp)
-        par_g['i_ext'] = input_current
-        st_mon = simulate_GPe_cell(par_g, par_sim)
-        plot_data(st_mon, ax[ii])
-        plot_current(st_mon, ax[3], [-1, 22])
-    plt.tight_layout()
-    plt.savefig('figure_2.png')
-    plt.close()
+    def simulate_1_gpe_cell():
+        start_time = time()
+        current_unit = b2.pA
+        # Figure 2 ------------------------------------------------------
+        fig, ax = plt.subplots(5, figsize=(10, 8))
+        i_stim = [20, 0, -0.5, 170.]
+        for ii in range(len(i_stim)):
 
-    print("Done in {} seconds".format(time() - start_time))
-    
+            input_current = get_step_current(0,
+                                             1000,
+                                             b2.ms,
+                                             i_stim[ii] * current_unit)
+            if ii == 3:
+                input_current = get_step_current(100,
+                                                 250,
+                                                 b2.ms,
+                                                 i_stim[ii] * current_unit)
+                par_sim['simulation_time'] = 350 * b2.ms
+
+            par_g['i_ext'] = input_current
+            st_mon = simulate_GPe_cell(par_g, par_sim)
+            plot_data(st_mon, ax[ii], title=str(i_stim[ii]))
+            plot_current(st_mon, current_unit, ax[-1], [-1, 22])
+
+        print("Done in {} seconds".format(time() - start_time))
+        plt.tight_layout()
+        plt.savefig('figure_2.png')
+        plt.close()
+
+    def simulate_2_gpe_cell():
+        start_time = time()
+        current_unit = b2.pA
+        par_sim['simulation_time'] = 300 * b2.ms
+        fig, ax = plt.subplots(3, figsize=(10, 6), sharex=True)
+
+        par_g['iapp'] = -0.5 *b2.pA
+        st_mon = simulate_two_GPe_cell(par_g, par_sim)
+        plot_data(st_mon, ax[0])
+        ax[0].plot(st_mon.t / b2.ms, st_mon.vg[0] /
+                   b2.mV, lw=2, c='b', label="0")
+        ax[0].plot(st_mon.t / b2.ms, st_mon.vg[1] /
+                   b2.mV, lw=2, c='r', label="1")
+        ax[1].plot(st_mon.t/b2.ms, st_mon.s_gg[0], lw=2, c='b', label="0")
+        ax[1].plot(st_mon.t/b2.ms, st_mon.s_gg[1], lw=2, c='r', label="1")
+        ax[0].legend()
+        ax[1].legend()
+        ax[0].set_xlim(0, np.max(st_mon.t / b2.ms))
+        ax[0].set_ylabel("v [mV]", fontsize=14)
+        ax[1].set_ylabel("s_syn", fontsize=14)
+
+        ax[-1].plot(st_mon.t / b2.ms,
+                    st_mon.i_syn_gg[1] / current_unit, lw=1, c='k', alpha=0.5)
+        ax[-1].set_xlabel("t [ms]", fontsize=14)
+        ax[-1].set_ylabel("I_syn [{}]".format(str(current_unit)), fontsize=14)
+        # ax[-1].set_xlim(290, 310)
+
+        print("Done in {} seconds".format(time() - start_time))
+        plt.tight_layout()
+        plt.savefig('figure_2_gpe.png')
+        plt.close()
+
+    # simulate_1_gpe_cell()
+    simulate_2_gpe_cell()
