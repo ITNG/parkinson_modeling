bc3b409226c4bd400592ea6f6d68a11dab9f2959
Abolfazl Ziaeemehr <a.ziaeemehr@gmail.com>
2021-01-06 17:19:26 +0330

    stn cell, two cell added, unit dimensions modified.

--- terman2002/Brian/stn_cell/lib.py
+++ terman2002/Brian/stn_cell/lib.py
@@ -1,6 +1,173 @@
+from brian2.equations import refractory
 import numpy as np
 import pylab as plt
 import brian2 as b2
+
+# -------------------------------------------------------------------
+
+
+def simulate_2_STN_cell_simpl_biexp(par, par_sim):
+
+    """
+    without considering Hinf in synapse equation
+    """
+
+    num = par['num']
+
+    eqs = '''
+    I_ext : amp 
+
+    minf = 1/(1+exp(-(vs-thetam*mV)/(sigmam*mV))) : 1
+    hinf = 1/(1+exp(-(vs-thetah*mV)/(sigmah*mV))) : 1 
+    ninf = 1/(1+exp(-(vs-thetan*mV)/(sigman*mV))) : 1
+    ainf = 1/(1+exp(-(vs-thetaa*mV)/(sigmaa*mV))) : 1
+    binf = 1/(1+exp((r-thetab)/sigmab))-1/(1+exp(-thetab/sigmab)) : 1
+    rinf = 1/(1+exp(-(vs-thetar*mV)/(sigmar*mV))) : 1
+    sinf = 1/(1+exp(-(vs-thetas*mV)/(sigmas*mV))) : 1
+    taun = taun0+taun1/(1+exp(-(vs-thn*mV)/(sigmant*mV))) : second
+    tauh = tauh0+tauh1/(1+exp(-(vs-thh*mV)/(sigmaht*mV))) : second
+    taur = taur0+taur1/(1+exp(-(vs-thr*mV)/(sigmart*mV))) : second
+
+    il = gl * (vs - vl) : amp
+    ina = gna * minf ** 3 * h * (vs - vna) : amp
+    ik = gk * n ** 4 * (vs - vk) : amp
+    iahp = gahp * ca / (ca + k1) * (vs - vk) : amp 
+    ica = gca * sinf ** 2 * (vs - vca) : amp 
+    it = gt * ainf ** 3 * binf ** 2 * (vs - vca) : amp 
+    i_syn_ss = g_syn_ss * (v_rev_ss - vs)  :amp
+
+    ds_ss/dt = - s_ss * beta : siemens
+    dg_syn_ss/dt = scale_f * (s_ss - g_syn_ss) * alpha : siemens
+
+    dh/dt  = phi * (hinf - h) / tauh  : 1
+    dn/dt  = phi * (ninf - n) / taun  : 1
+    dr/dt  = phir * (rinf - r) / taur : 1
+    dca/dt = eps * ((-ica - it)/pA - kca* ca) : 1  #! pA
+    membrane_Im = -(il + ina + ik + it + ica + iahp) + I_ext + i_syn_ss : amp
+        
+    dvs/dt = membrane_Im/C : volt
+    '''
+
+    neurons = b2.NeuronGroup(2,
+                             eqs,
+                             method=par_sim['integration_method'],
+                             dt=par_sim['dt'],
+                             threshold='vs>-20*mV',
+                             refractory='vs>-20*mV',
+                             namespace=par,
+                             )
+
+    syn_ss = b2.Synapses(neurons, neurons,
+                         on_pre="s_ss +={}*nS".format(par["w"]),
+                         dt=par_sim['dt'],
+                         method=par_sim['integration_method'],
+                         namespace=par
+                         )
+    syn_ss.connect(i=0, j=1)
+
+    neurons.vs = par['v0']
+    neurons.h = "hinf"
+    neurons.n = "ninf"
+    neurons.r = "rinf"
+    neurons.ca = 0
+    neurons.I_ext = 0*b2.pA
+
+    st_mon = b2.StateMonitor(neurons,
+                             ["vs", "i_syn_ss", "g_syn_ss"],
+                             record=True)
+
+    net = b2.Network(neurons)
+    net.add(st_mon)
+    net.add(syn_ss)
+    net.run(par_sim['simulation_time'])
+
+    return st_mon
+# -------------------------------------------------------------------
+
+
+def simulate_2_STN_cell_biexp(par, par_sim):
+
+    """
+    considering Hinf in synapse equation
+    """
+
+    num = par['num']
+    b2.defaultclock.dt = par_sim['dt']
+
+
+    eqs = '''
+    I_ext : amp 
+
+    minf = 1/(1+exp(-(vs-thetam*mV)/(sigmam*mV))) : 1
+    hinf = 1/(1+exp(-(vs-thetah*mV)/(sigmah*mV))) : 1 
+    ninf = 1/(1+exp(-(vs-thetan*mV)/(sigman*mV))) : 1
+    ainf = 1/(1+exp(-(vs-thetaa*mV)/(sigmaa*mV))) : 1
+    binf = 1/(1+exp((r-thetab)/sigmab))-1/(1+exp(-thetab/sigmab)) : 1
+    rinf = 1/(1+exp(-(vs-thetar*mV)/(sigmar*mV))) : 1
+    sinf = 1/(1+exp(-(vs-thetas*mV)/(sigmas*mV))) : 1
+    taun = taun0+taun1/(1+exp(-(vs-thn*mV)/(sigmant*mV))) : second
+    tauh = tauh0+tauh1/(1+exp(-(vs-thh*mV)/(sigmaht*mV))) : second
+    taur = taur0+taur1/(1+exp(-(vs-thr*mV)/(sigmart*mV))) : second
+
+    il = gl * (vs - vl) : amp
+    ina = gna * minf ** 3 * h * (vs - vna) : amp
+    ik = gk * n ** 4 * (vs - vk) : amp
+    iahp = gahp * ca / (ca + k1) * (vs - vk) : amp 
+    ica = gca * sinf ** 2 * (vs - vca) : amp 
+    it = gt * ainf ** 3 * binf ** 2 * (vs - vca) : amp 
+    i_syn_ss : amp
+
+    Hinf = 1./(1+exp(-(vs - thetag * mV - thetagH*mV) / (sigmagH*mV))):1
+    ds_ss/dt = alpha * Hinf * (1 - s_ss) - beta * s_ss  : 1
+    # ds_ss/dt = 0.5 * (1 + tanh(0.1*vs/mV)) * alpha * (1 - s_ss) - beta * s_ss  : 1
+
+    dh/dt  = phi * (hinf - h) / tauh  : 1
+    dn/dt  = phi * (ninf - n) / taun  : 1
+    dr/dt  = phir * (rinf - r) / taur : 1
+    dca/dt = eps * ((-ica - it)/pA - kca* ca) : 1  #! pA
+    membrane_Im = -(il + ina + ik + it + ica + iahp) + I_ext + i_syn_ss : amp
+        
+    dvs/dt = membrane_Im/C : volt
+    '''
+
+    eqs_syn_ss = '''  # synapse model, s to s
+    i_syn_ss_post = w_ss * s_ss_pre * (v_rev_ss - vs) :amp (summed)
+    '''
+    neurons = b2.NeuronGroup(2,
+                             eqs,
+                             method=par_sim['integration_method'],
+                             dt=par_sim['dt'],
+                             threshold='vs>-20*mV',
+                             refractory='vs>-20*mV',
+                             namespace=par,
+                             )
+
+    syn_ss = b2.Synapses(neurons, neurons,
+                        eqs_syn_ss,
+                         dt=par_sim['dt'],
+                         method=par_sim['integration_method'],
+                         namespace=par,
+                         )
+    syn_ss.connect(i=0, j=1)
+
+    neurons.vs = par['v0']
+    neurons.h = "hinf"
+    neurons.n = "ninf"
+    neurons.r = "rinf"
+    neurons.ca = 0
+    neurons.I_ext = 0*b2.pA
+
+    st_mon = b2.StateMonitor(neurons,
+                             ["vs", "i_syn_ss", "s_ss"],
+                             record=[0, 1])
+
+    net = b2.Network(neurons)
+    net.add(st_mon)
+    net.add(syn_ss)
+    net.run(par_sim['simulation_time'])
+
+    return st_mon
+# -------------------------------------------------------------------
 
 
 def simulate_STN_cell(par, par_sim):
@@ -32,7 +199,7 @@ def simulate_STN_cell(par, par_sim):
     dh/dt  = phi * (hinf - h) / tauh  : 1
     dn/dt  = phi * (ninf - n) / taun  : 1
     dr/dt  = phir * (rinf - r) / taur : 1
-    dca/dt = eps * ((-ica - it)/uamp - kca* ca) : 1 
+    dca/dt = eps * ((-ica - it)/pA - kca* ca) : 1    #! pA
     membrane_Im = -(il + ina + ik + it + ica + iahp) + I_ext  : amp
         
     dvs/dt = membrane_Im/C : volt
@@ -60,18 +227,24 @@ def simulate_STN_cell(par, par_sim):
     net.run(par_sim['simulation_time'])
 
     return st_mon
+# -------------------------------------------------------------------
 
 
-def plot_data(st_mon, ax):
-    ax.plot(st_mon.t / b2.ms, st_mon.vs[0] / b2.mV, lw=2, c='k')
+def plot_data(st_mon, ax, index=0):
+    ax.plot(st_mon.t / b2.ms, st_mon.vs[index] / b2.mV, lw=2, c='k')
     ax.set_xlim(0, np.max(st_mon.t / b2.ms))
     # ax.set_xlabel("time [ms]", fontsize=14)
     ax.set_ylabel("v [mV]", fontsize=14)
+# -------------------------------------------------------------------
 
 
-def plot_current(st_mon, ax):
-    ax.plot(st_mon.t / b2.ms, st_mon.I_ext[0] / b2.uamp, lw=1, c='k', alpha=0.5)
+def plot_current(st_mon, ax, current_unit=b2.pA):
+    ax.plot(st_mon.t / b2.ms,
+            st_mon.I_ext[0] / current_unit, lw=1, c='k', alpha=0.5)
+
+    ylabel = "I [{}]".format(str(current_unit))
+
     ax.set_xlabel("t [ms]", fontsize=14)
-    ax.set_ylabel("I [micro A]", fontsize=14)
-    I = st_mon.I_ext[0] / b2.uamp
+    ax.set_ylabel(ylabel, fontsize=14)
+    I = st_mon.I_ext[0] / current_unit
     ax.set_ylim(np.min(I)*1.1, np.max(I)*1.1)
--- terman2002/Brian/stn_cell/main.py
+++ terman2002/Brian/stn_cell/main.py
@@ -3,6 +3,8 @@ import brian2 as b2
 import pylab as plt
 from time import time
 from lib import (simulate_STN_cell,
+                 simulate_2_STN_cell_biexp,
+                 simulate_2_STN_cell_simpl_biexp,
                  plot_data,
                  plot_current)
 from input_factory import get_step_current
@@ -16,12 +18,12 @@ if __name__ == "__main__":
         'vna': 55 * b2.mV,
         'vk': -80 * b2.mV,
         'vca': 140 * b2.mV,
-        'gl': 2.25 * b2.msiemens,
-        'gna': 37.5 * b2.msiemens,
-        'gk': 45 * b2.msiemens,
-        'gahp': 9 * b2.msiemens,
-        'gca': 0.5 * b2.msiemens,
-        'gt': 0.5 * b2.msiemens,
+        'gl': 2.25 * b2.nS,
+        'gna': 37.5 * b2.nS,
+        'gk': 45 * b2.nS,
+        'gahp': 9 * b2.nS,
+        'gca': 0.5 * b2.nS,
+        'gt': 0.5 * b2.nS,
         'thetam': -30,
         'thetas': -39,
         'thetah': -39,
@@ -33,7 +35,7 @@ if __name__ == "__main__":
         'sigmah': -3.1,
         'sigman': 8,
         'sigmaa': 7.8,
-        'sigmab': -0.1, # Guo 0.07 Terman02 -0.1
+        'sigmab': -0.1,  # Guo 0.07 Terman02 -0.1
         'sigmam': 15,
         'sigmar': -2,
         'sigmaht': -3,
@@ -45,10 +47,10 @@ if __name__ == "__main__":
         'taur1': 17.5 * b2.ms,
         'tauh0': 1 * b2.ms,
         'tauh1': 500 * b2.ms,
-        'eps': 3.75e-05 / b2.ms, # 1/ms Guo 0.00005 Terman02 0.0000375
-        'alpha': 5,
-        'beta': 1,
-        'phir': 0.2, # Guo 0.5 Terman02 0.2
+        'eps': 3.75e-05 / b2.ms,  # 1/ms Guo 0.00005 Terman02 0.0000375
+        'alpha': 5./b2.ms,
+        'beta': 1./b2.ms,
+        'phir': 0.2,  # Guo 0.5 Terman02 0.2
         'phi': 0.75,
         'kca': 22.5,
         'thn': -80,
@@ -57,48 +59,132 @@ if __name__ == "__main__":
         'ab': -30,
         'k1': 15,
         # 'i_ext': 0 * b2.uamp,
-        'C': 1 * b2.ufarad,
+        'C': 1 * b2.pF,
+        'v_rev_ss': 0. * b2.mV,
+        'w': 1.0,  # *b2.nS
+        'w_ss':1.0*b2.nS,
+        'thetag':30.,
+        'thetagH':-39.,
+        'sigmagH':8.,
     }
+    tau_1 = 1.0/par['alpha']
+    tau_2 = 1.0/par['beta']
+    par['scale_f'] = (tau_2 / tau_1) ** (tau_1 / (tau_2 - tau_1))
 
     par_sim = {
         'integration_method': "rk4",
-        'simulation_time': 2500 * b2.ms,
+        'simulation_time': 2500 * b2.ms,  # 2500 * b2.ms,
         'dt': 0.05 * b2.ms,
     }
 
-    start_time = time()
+    
 
     # Figure 1e -----------------------------------------------------
-    # fig, ax = plt.subplots(4, figsize=(10, 6), sharex=True)
-    # t_stim = [300, 450, 600]
-    # for ii in range(len(t_stim)):
-    #     input_current = get_step_current(500,
-    #                                      500 + t_stim[ii],
-    #                                      b2.ms,
-    #                                      -25 * b2.uamp)
-    #     par['i_ext'] = input_current
-    #     st_mon = simulate_STN_cell(par, par_sim)
-    #     plot_data(st_mon, ax[ii])
-    #     plot_current(st_mon, ax[3])
-    # plt.tight_layout()
-    # plt.savefig('figure_1e.png')
-    # plt.close()
+    def plot_fig_1e():
+        start_time = time()
+        fig, ax = plt.subplots(4, figsize=(10, 6), sharex=True)
+        t_stim = [300, 450, 600]
+        for ii in range(len(t_stim)):
+            input_current = get_step_current(500,
+                                             500 + t_stim[ii],
+                                             b2.ms,
+                                             -25 * b2.pA)
+            par['i_ext'] = input_current
+            st_mon = simulate_STN_cell(par, par_sim)
+            plot_data(st_mon, ax[ii])
+            plot_current(st_mon, ax[3])
+        print("Done in {}".format(time() - start_time))
+        plt.tight_layout()
+        plt.savefig('figure_1e.png')
+        plt.close()
 
     # Figure 1f -----------------------------------------------------
-    fig, ax = plt.subplots(4, figsize=(10, 6), sharex=True)
-    i_stim = [-20, -30, -40]
-    for ii in range(len(i_stim)):
-        input_current = get_step_current(500,
-                                         800,
-                                         b2.ms,
-                                         i_stim[ii] * b2.uamp)
-        par['i_ext'] = input_current
-        st_mon = simulate_STN_cell(par, par_sim)
-        plot_data(st_mon, ax[ii])
-        plot_current(st_mon, ax[3])
-    plt.tight_layout()
-    plt.savefig('figure_1f.png')
-    plt.close()
+    def plot_fig_1f():
+        current_unit = b2.pA
+        start_time = time()
+        fig, ax = plt.subplots(4, figsize=(10, 6), sharex=True)
+        i_stim = [-20, -30, -40]
+        for ii in range(len(i_stim)):
+            input_current = get_step_current(500,
+                                             800,
+                                             b2.ms,
+                                             i_stim[ii] * current_unit)
+            par['i_ext'] = input_current
+            st_mon = simulate_STN_cell(par, par_sim)
+            plot_data(st_mon, ax[ii])
+            plot_current(st_mon, ax[3], current_unit)
+        print("Done in {}".format(time() - start_time))
+        plt.tight_layout()
+        plt.savefig('figure_1f.png')
+        plt.close()
 
+    def two_stn_cell_simpl():
+        current_unit = b2.pA
+        par_sim['simulation_time'] = 350. * b2.ms
+        start_time = time()
+        fig, ax = plt.subplots(3, figsize=(10, 5), sharex=True)
+        i_stim = [0.]
+        for ii in range(len(i_stim)):
+            
+            st_mon = simulate_2_STN_cell_simpl_biexp(par, par_sim)
+            plot_data(st_mon, ax[ii], index=1)
+            ax[0].plot(st_mon.t / b2.ms, st_mon.vs[0] /
+                       b2.mV, lw=2, c='b', label="0")
+            ax[0].plot(st_mon.t / b2.ms, st_mon.vs[1] /
+                       b2.mV, lw=2, c='r', label="1")
+            ax[1].plot(st_mon.t/b2.ms, st_mon.g_syn_ss[1]/b2.nS, lw=2, c='k')
+            ax[0].legend()
+            ax[0].set_xlim(0, np.max(st_mon.t / b2.ms))
+            ax[0].set_ylabel("v [mV]", fontsize=14)
+            ax[1].set_ylabel("g_syn [mV]", fontsize=14)
+
+            ax[-1].plot(st_mon.t / b2.ms,
+                        st_mon.i_syn_ss[1] / current_unit, lw=1, c='k', alpha=0.5)
+            ax[-1].set_xlabel("t [ms]", fontsize=14)
+            ax[-1].set_ylabel("I [{}]".format(str(current_unit)), fontsize=14)
+            ax[-1].set_xlim(300, 330)
+        
+        print("Done in {}".format(time() - start_time))
+        plt.tight_layout()
+        plt.savefig('figure_2_stn_simpl.png')
+        plt.show()
+        # plt.close()
     
-    print("Done in {}".format(time() - start_time))
+    def two_stn_cell():
+        current_unit = b2.pA
+        par_sim['simulation_time'] = 350 * b2.ms
+        start_time = time()
+        fig, ax = plt.subplots(3, figsize=(10, 5), sharex=True)
+        i_stim = [0.]
+        for ii in range(len(i_stim)):
+            
+            st_mon = simulate_2_STN_cell_biexp(par, par_sim)
+            plot_data(st_mon, ax[ii], index=1)
+            ax[0].plot(st_mon.t / b2.ms, st_mon.vs[0] /
+                       b2.mV, lw=2, c='b', label="0")
+            ax[0].plot(st_mon.t / b2.ms, st_mon.vs[1] /
+                       b2.mV, lw=2, c='r', label="1")
+            ax[1].plot(st_mon.t/b2.ms, st_mon.s_ss[0], lw=2, c='b', label="0")
+            ax[1].plot(st_mon.t/b2.ms, st_mon.s_ss[1], lw=2, c='r', label="1")
+            ax[0].legend()
+            ax[1].legend()
+            ax[0].set_xlim(0, np.max(st_mon.t / b2.ms))
+            ax[0].set_ylabel("v [mV]", fontsize=14)
+            ax[1].set_ylabel("s_syn", fontsize=14)
+
+            ax[-1].plot(st_mon.t / b2.ms,
+                        st_mon.i_syn_ss[1] / current_unit, lw=1, c='k', alpha=0.5)
+            ax[-1].set_xlabel("t [ms]", fontsize=14)
+            ax[-1].set_ylabel("I_syn [{}]".format(str(current_unit)), fontsize=14)
+            ax[-1].set_xlim(300, 330)
+        
+        print("Done in {}".format(time() - start_time))
+        plt.tight_layout()
+        plt.savefig('figure_2_stn.png')
+        plt.show()
+        # plt.close()
+
+    plot_fig_1e()
+    plot_fig_1f()
+    two_stn_cell_simpl()
+    two_stn_cell()
