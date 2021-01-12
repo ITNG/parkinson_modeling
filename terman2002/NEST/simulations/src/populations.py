681b3b189755816b79e111415c2a8cc6dca7f420
Abolfazl Ziaeemehr <a.ziaeemehr@gmail.com>
2021-01-12 12:50:49 +0330

    joblib added but does not work for un pickleable error.

--- terman2002/NEST/simulations/src/populations.py
+++ terman2002/NEST/simulations/src/populations.py
@@ -4,6 +4,7 @@ import time
 import numpy as np
 import pylab as plt
 from os.path import join
+from copy import deepcopy
 from numpy.random import rand
 from joblib import Parallel, delayed
 np.random.seed(5)
@@ -61,19 +62,17 @@ class TER_RUB():
                               'rng_seeds': master_seed_range2})
     # ---------------------------------------------------------------
 
-    def set_params(self, par_stn, par_gpe,
-                   par_syn_SG, par_syn_GS, par_syn_GG,
-                   par_simulation):
+    def set_params(self, params):
 
-        self.dt = par_simulation['dt']
-        self.state = par_simulation['state']
+        self.par_syn_StoG = params['par_syn_StoG']
+        self.par_syn_GtoS = params['par_syn_GtoS']
+        self.par_syn_GtoG = params['par_syn_GtoG']
+        self.par_syn_common = par_syn_common  # read from global variable
+        self.par_simulation = par_simulation  # read from global variable
+
         self.t_simulation = par_simulation['t_simulation']
-        self.par_stn = par_stn
-        self.par_gpe = par_gpe
-        self.par_syn_SG = par_syn_SG
-        self.par_syn_GS = par_syn_GS
-        self.par_syn_GG = par_syn_GG
-        self.par_simulation = par_simulation
+        self.state = par_simulation['state']
+        self.dt = par_simulation['dt']
     # ---------------------------------------------------------------
 
     def build(self):
@@ -99,8 +98,8 @@ class TER_RUB():
             nest.SetDefaults(self.stn_model_name, dict_default_stn)
             nest.SetDefaults(self.gpe_model_name, dict_default_gpe)
 
-        nest.CopyModel(self.stn_model_name, "STN_model", self.par_stn)
-        nest.CopyModel(self.gpe_model_name, "GPe_model", self.par_gpe)
+        nest.CopyModel(self.stn_model_name, "STN_model", self.par_syn_common)
+        nest.CopyModel(self.gpe_model_name, "GPe_model", self.par_syn_common)
 
         self.stn_cells = nest.Create("STN_model", n_stn)
         self.gpe_cells = nest.Create("GPe_model", n_gpe)
@@ -154,18 +153,18 @@ class TER_RUB():
         nest.Connect(self.gpe_cells, self.gpe_cells,
                      conn_spec=conn_dict_GG,
                      syn_spec={"receptor_type": 4,
-                               'weight': self.par_syn_GG['weight'],
-                               'delay': self.par_syn_GG['delay']})
+                               'weight': self.par_syn_GtoG['weight'],
+                               'delay': self.par_syn_GtoG['delay']})
         nest.Connect(self.stn_cells, self.gpe_cells,
                      conn_spec=conn_dict_SG,
                      syn_spec={"receptor_type": 1,
-                               'weight': self.par_syn_SG['weight'],
-                               'delay': self.par_syn_SG['delay']})
+                               'weight': self.par_syn_StoG['weight'],
+                               'delay': self.par_syn_StoG['delay']})
         nest.Connect(self.gpe_cells, self.stn_cells,
                      conn_spec=conn_dict_GS,
                      syn_spec={"receptor_type": 3,
-                               'weight': self.par_syn_GS['weight'],
-                               'delay': self.par_syn_GS['delay']})
+                               'weight': self.par_syn_GtoS['weight'],
+                               'delay': self.par_syn_GtoS['delay']})
 
         nest.Connect(self.stn_multimeter, self.stn_cells, "one_to_one")
         nest.Connect(self.gpe_multimeter, self.gpe_cells, "one_to_one")
@@ -190,8 +189,8 @@ class TER_RUB():
             print("network not connected.")
             exit(0)
 
-        print("g_SG = {:10.3f}, g_GG = {:10.3f}".format(self.par_syn_SG['weight'],
-                                                        self.par_syn_GG['weight']))
+        print("g_SG = {:10.3f}, g_GG = {:10.3f}".format(self.par_syn_StoG['weight'],
+                                                        self.par_syn_GtoG['weight']))
 
         t_simulation = self.par_simulation['t_simulation']
         t_transition = self.par_simulation['t_transition']
@@ -269,20 +268,8 @@ def display_time(time):
 
 if __name__ == "__main__":
 
-    par_stn = {
-        "AMPA_Tau_1": 0.2,
-        "AMPA_Tau_2": 1.0,
-        "AMPA_E_rev": 0.0,
-        "GABA_A_Tau_1": 0.5,
-        "GABA_A_Tau_2": 12.5,
-        "GABA_A_E_rev": -85.0,
-        "GABA_B_Tau_1": 0.5,
-        "GABA_B_Tau_2": 12.5,
-        "GABA_B_E_rev": -100.0,
-    }
-
     #  I use Gaba_B as Gaba_A, for GG connection
-    par_gpe = {
+    par_syn_common = {
         "AMPA_Tau_1": 0.2,
         "AMPA_Tau_2": 1.0,
         "AMPA_E_rev": 0.0,
@@ -294,11 +281,8 @@ if __name__ == "__main__":
         "GABA_B_E_rev": -100.0,
     }
 
-    par_syn_GS = {'delay': 1.0, 'weight': 1.0}
-    par_syn_SG = {'delay': 1.0, 'weight': 0.03}
-    par_syn_GG = {'delay': 1.0, 'weight': 0.06}
     par_simulation = {
-        "dt": 0.1,
+        "dt": 0.5,
         'state': "Te2002",
         't_transition': 100.,
         't_simulation': 2000.,
@@ -306,30 +290,74 @@ if __name__ == "__main__":
         'n_gpe': 10,
     }
 
+    par_syn_GtoS = {'delay': 1.0, 'weight': 3.0}
+    par_syn_StoG = {'delay': 1.0, 'weight': 0.03}
+    par_syn_GtoG = {'delay': 1.0, 'weight': 0.06}
+
     start_time = time.time()
     install_modules()
 
-    g_SG = np.linspace(0.01, 2.0, 11)
-    g_GG = np.linspace(0.01, 2.0, 11)
 
-    for i in range(len(g_SG)):
-        for j in range(len(g_GG)):
+    def run_command(args):
 
-            sub_name = "{:.6f}-{:.6f}".format(g_SG[i], g_GG[j])
+        sol = TER_RUB(par_simulation['dt'])
+        sol.set_params(args)
+        sol.build()
+        sol.connect()
+        sol.run()
+        i = args['par_syn_StoG']['weight']
+        j = args['par_syn_GtoS']['weight']
+        sub_name = "{:.6f}-{:.6f}".format(i, j)
+        sol.plot_voltages(filename="v-"+sub_name)
+        
+        del sol
+
+    def batch_run(n_jobs):
+
+        args = []  # list of dictionary of dictionaries
+        for i in range(len(g_StoG)):
+            for j in range(len(g_GtoG)):
+                par_syn_StoG['weight'] = g_StoG[i]
+                par_syn_GtoG['weight'] = g_GtoG[j]
+                tmp = {
+                    "par_syn_StoG": par_syn_StoG,
+                    "par_syn_GtoS": par_syn_GtoS,
+                    "par_syn_GtoG": par_syn_GtoG,
+                }
+                args.append(deepcopy(tmp))
+        
+        Parallel(n_jobs=n_jobs)(map(delayed(run_command), args))
+
+
+    g_StoG = np.linspace(0.01, 2.0, 3)
+    g_GtoG = np.linspace(0.01, 2.0, 3)
+    batch_run(2)
+
+    exit(0)
+
+    # serial running
+    params = {
+        "par_syn_StoG": par_syn_StoG,
+        "par_syn_GtoS": par_syn_GtoS,
+        "par_syn_GtoG": par_syn_GtoG,
+    }
+
+    for i in range(len(g_StoG)):
+        for j in range(len(g_GtoG)):
+
+            sub_name = "{:.6f}-{:.6f}".format(g_StoG[i], g_GtoG[j])
 
             sol = TER_RUB(par_simulation['dt'])
-            par_syn_SG['weight'] = g_SG[i]
-            par_syn_GG['weight'] = g_GG[j]
-            sol.set_params(par_stn,
-                           par_gpe,
-                           par_syn_SG,
-                           par_syn_GS,
-                           par_syn_GG,
-                           par_simulation)
+            
+            # these update the params as well by reference
+            par_syn_StoG['weight'] = g_StoG[i]
+            par_syn_GtoG['weight'] = g_GtoG[j]
+
+            sol.set_params(params)
             sol.build()
             sol.connect()
             sol.run()
-            sol.plot_raster(filename="s-"+sub_name)
+            # sol.plot_raster(filename="s-"+sub_name)
             sol.plot_voltages(filename="v-"+sub_name)
 
     display_time(time.time() - start_time)
