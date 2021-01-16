fe03c3a3bfd51d9b738ea4607ef1482fb3187baa
Abolfazl Ziaeemehr <a.ziaeemehr@gmail.com>
2021-01-16 21:01:59 +0330

    Thl cell modified.

--- TermanRubin2004/Brian/th_cell/lib.py
+++ TermanRubin2004/Brian/th_cell/lib.py
@@ -1,49 +1,110 @@
 import numpy as np
 import pylab as plt
 import brian2 as b2
+from matplotlib.gridspec import GridSpec
+import os
+
+if not os.path.exists("data"):
+    os.makedirs("data")
 
 
-def simulate_Th_cell(par, par_sim):
-    num = par['num']
-    input_current = par['i_ext']
+def simulate_Thl_cell(par, par_sim):
+    num = par_sim['num']
+    i_sensory_motor=par_sim['I_sm']
 
-    eqs = '''
+    # b2.set_device('cpp_standalone')
+    b2.start_scope()
 
-    minftc  =  1./(1+exp(-(vt+37.*mV)/(7.*mV))) : 1
-    pinftc  = 1./(1.+exp(-(vt+60.*mV)/(6.2*mV))) : 1
-    ahtc =  0.128*exp(-(46.*mV+vt)/(18.*mV)) : 1
-    bhtc =  4./(1+exp(-(23.*mV+vt)/(5.*mV))) : 1
-    hinftc  =  1./(1.+exp((vt+41.*mV)/(4.*mV))) : 1
-    rinftc  = 1./(1.+exp((vt+84.*mV)/(4.*mV))) : 1
-    tauhtc = 1./(ahtc+bhtc) *ms                    : second
-    taurtc = (28.+ exp((vt+25.*mV)/(-10.5*mV)))*ms : second
-    
-    iltc = glbar*(vt-eleak) : amp 
-    inatc = gnabar*minftc*minftc*minftc*htc*(vt-ena) : amp
-    n2tc = .75*(1-htc) * 0.75*(1-htc) : 1
-    n4tc = n2tc * n2tc : 1
-    iktc = gkbar*n4tc*(vt-ek) : amp
-    ittc= gtbar*pinftc*pinftc*htc*vt : amp 
+    eqs = '''    
+    minfthl = 1/(1+exp(-(vt-thtmthl*mV)/(sigmthl*mV))): 1
+    hinfthl =  1/(1+exp((vt-thththl*mV)/(sighthl*mV))): 1
+    pinfthl = 1/(1+exp(-(vt-thtpthl*mV)/(sigpthl*mV))) :1
+    rinfthl = 1/(1+exp((vt-thtrthl*mV)/(sigrthl*mV))) :1
+    ahthl =  ah0thl*exp(-(vt-thtahthl*mV)/(sigahthl*mV)) :1
+    bhthl =  bh0thl/(1+exp(-(vt-thtbhthl*mV)/(sigbhthl*mV))) :1
+    tauhthl =  1/(ahthl+bhthl) *ms :second
+    taurthl = taur0thl+taur1thl*exp(-(vt-thtrtauthl*mV)/(sigrtauthl*mV)) :second
 
-    membrane_Itc = -(iltc+inatc+iktc+ittc) + iexttc : amp
+    ilthl=glthl*(vt-vlthl):amp
+    inathl=gnathl*minfthl*minfthl*minfthl*hthl*(vt-vnathl) :amp
+    ikthl=gkthl*((0.75*(1-hthl))**4)*(vt-vkthl) :amp
+    itthl=gtthl*pinfthl*pinfthl*rthl*(vt-vtthl) :amp
+    iextthl:amp
+    ithl_sm = i_sensory_motor(t) : amp
 
-    dhtc/dt = (hinftc - htc) / tauhtc : 1
-    drtc/dt = (rinftc - rtc) / taurtc
-    dvt/dt = membrane_Itc / Cm : volt
-
+    membrane_Ithl = -(ilthl+inathl+ikthl+itthl)+iextthl+ithl_sm :amp
+    drthl/dt=phirthl*(rinfthl-rthl)/taurthl :1
+    dhthl/dt=phihthl*(hinfthl-hthl)/tauhthl :1 
+    dvt/dt = membrane_Ithl / cmthl : volt
     '''
+
+    # ym=1/(1+exp(-v/sigym)):1
+    # ismthl(t)=imsmthl*ym(sin(2*pi*(t-dsmthl)/tmsmthl))*(1-ym(sin(2*pi*(t-dsmthl+wsmthl)/tmsmthl)))
+    
 
     neuron = b2.NeuronGroup(num,
                             eqs,
                             method=par_sim['integration_method'],
                             dt=par_sim['dt'],
-                            threshold='vs>-20*mV',
-                            refractory='vs>-20*mV',
+                            threshold='vt>-55*mV',
+                            refractory='vt>-55*mV',
                             namespace=par,
                             )
 
-    neuron.vs = par['v0']
-    neuron.h = "hinf"
-    neuron.n = "ninf"
-    neuron.r = "rinf"
-    neuron.ca = 0
+    neuron.vt = par['v0']
+    neuron.hthl = "hinfthl"
+    neuron.rthl = "rinfthl"
+    neuron.iextthl = par['iext']
+
+    state_monitor = b2.StateMonitor(neuron, ["vt", "ithl_sm"], record=True)
+
+    net = b2.Network(neuron)
+    net.add(state_monitor)
+    net.run(par_sim['simulation_time'])
+
+    return state_monitor
+
+
+def plot_data(state_monitor, ax, index=0, xlabel=None):
+    ax.plot(state_monitor.t / b2.ms, state_monitor.vt[index] / b2.mV,
+            lw=1, c='k')
+    ax.set_xlim(0, np.max(state_monitor.t / b2.ms))
+    if xlabel is not None:
+        ax.set_xlabel(xlabel, fontsize=12)
+    ax.set_ylabel("v [mV]", fontsize=12)
+    # plt.show()
+
+
+def plot_current(state_monitor, ax, current_unit=b2.pA, xlabel=None):
+    ax.plot(state_monitor.t / b2.ms,
+            state_monitor.ithl_sm[0] / current_unit, lw=1, c='k', alpha=0.5)
+
+    ylabel = "I [{}]".format(str(current_unit))
+
+    ax.set_ylabel(ylabel, fontsize=12)
+    I = state_monitor.ithl_sm[0] / current_unit
+    # ax.set_ylim(np.min(I)*1.1, np.max(I)*1.1)
+    if xlabel is not None:
+        ax.set_xlabel(xlabel)
+
+def make_grid(nrows, ncols, left=0.05, right=0.9,
+              bottom=0.05, top=0.95, hspace=0.2,
+              wspace=0.2):
+
+    gs = GridSpec(nrows, ncols)
+    gs.update(left=left, right=right,
+              hspace=hspace, wspace=wspace,
+              bottom=bottom, top=top)
+    ax = []
+    if nrows > 1:
+        for i in range(nrows):
+            ax_row = []
+            for j in range(ncols):
+                ax_row.append(plt.subplot(gs[i, j]))
+            ax.append(ax_row)
+    else:
+        for j in range(ncols):
+            ax.append(plt.subplot(gs[j]))
+
+    return [ax, gs]
+
--- TermanRubin2004/Brian/th_cell/run.py
+++ TermanRubin2004/Brian/th_cell/run.py
@@ -2,28 +2,110 @@ import numpy as np
 import brian2 as b2
 import pylab as plt
 from time import time
+from os.path import join
+from lib import plot_current, simulate_Thl_cell, plot_data, make_grid
+from matplotlib.gridspec import GridSpec
 
 par = {
-    'vsyntc': -85,
-    'gsyntc': .08,
-    'asg': 200,
-    'bsg': .4,
-    'itc': 6,
-    'shi': -80,
-    'shi2': -90,
-    'dur': 5,
-    'dur2': 10,
-    'period': 25,
-    'gnabar': 3,
-    'gkbar': 5,
-    'glbar': 0.05,
-    'gtbar': 5,
-    'ena': 50,
-    'ek': -90,
-    'eleak': -70,
-    'qht': 2.5,
-    'tadj': 1,
-    'apr': 4,
-    'apt': 1,
+    'v0': -65.*b2.mV,
+    'cmthl': 1*b2.pF,
+    'glthl': 0.05*b2.nS,
+    'gkthl': 5*b2.nS,
+    'gtthl': 5*b2.nS,
+    'gnathl': 3*b2.nS,
+
+    'vtthl': 0 * b2.mV,
+    'vlthl': -70 * b2.mV,
+    'vkthl': -90 * b2.mV,
+    'vnathl': 50 * b2.mV,
+
+    'sigmthl': 7,
+    'sighthl': 4,
+    'sigbhthl': 5,
+    'sigahthl': 18,
+    'sigpthl': 6.2,
+    'sigrthl': 4,
+
+    'thtmthl': -37,
+    'thththl': -41,
+    'thtahthl': -46,
+    'thtbhthl': -23,
+    'thtpthl': -60,
+    'thtrthl': -84,
+
+    'taur0thl': 28*b2.ms,
+    'ah0thl': 0.128,
+    'bh0thl': 4,
+    'phihthl': 1,
+    'iext': 0.45*b2.pA,
+
+}
+# sensorimotor control
+par_SM = {
+    'imsmthl': 8,
+    'tmsmthl': 25,
+    'wsmthl': 5,
+    'dsmthl': 80,
+    'sigym': 0.001,
 }
 
+T_CURRENT = 'fast'
+
+if T_CURRENT == 'fast':
+    par['taur1thl'] = 1*b2.ms
+    par['thtrtauthl'] = -25.
+    par['sigrtauthl'] = 10.5
+    par['phirthl'] = 2.5
+
+par_sim = {
+    'integration_method': "rk4",
+    'simulation_time': 1000 * b2.ms,  # 2500 * b2.ms,
+    'dt': 0.05 * b2.ms,
+    'num': 1,
+}
+
+if __name__ == "__main__":
+
+
+    def figure2():
+
+        fig = plt.figure(figsize=(10, 6))
+        gs = GridSpec(6, 1)
+        gs.update(left=0.1, right=0.95)
+        ax1 = plt.subplot(gs[:2])
+        ax2 = plt.subplot(gs[2])
+        ax3 = plt.subplot(gs[3:5])
+        ax4 = plt.subplot(gs[5])
+        ax = [ax1, ax2, ax3, ax4]
+        
+        start_time = time()
+
+        I_sm1 = b2.TimedArray([0.0, 2., 0., 5., 0., 10., 0.]*b2.pA, dt=100*b2.ms)
+        par_sim['I_sm'] = I_sm1
+        par['i_ext'] = 0.0
+        state_monitor = simulate_Thl_cell(par, par_sim)
+        print("Done in {}".format(time() - start_time))
+
+        plot_data(state_monitor, ax[0])
+        plot_current(state_monitor, ax[1])
+
+
+
+        I_sm2 = b2.TimedArray([0.0, -0.5, 0., -1., 0., ]*b2.pA, dt=150*b2.ms)
+        par_sim['I_sm'] = I_sm2
+        par['i_ext'] = 0.0
+        state_monitor = simulate_Thl_cell(par, par_sim)
+        print("Done in {}".format(time() - start_time))
+
+        plot_data(state_monitor, ax[2])
+        plot_current(state_monitor, ax[3], xlabel="Time [ms]")
+
+
+        [ax[i].set_xticks([]) for i in range(3)]
+        [ax[i].set_xlim([0, 1000]) for i in range(4)]
+        plt.savefig(join("data", "figure2.png"))
+        plt.show()
+
+
+
+    figure2()
