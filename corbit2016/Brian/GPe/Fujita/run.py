b9bcc98fb91dcab18584f28ea3467b220d97fafa
Ziaeemehr <a.ziaeemehr@gmail.com>
2021-02-27 13:32:36 +0330

    Fujita 2012 for GPe added.

---
+++ corbit2016/Brian/GPe/Fujita/run.py
@@ -0,0 +1,117 @@
+import os
+import numpy as np
+import brian2 as b2
+import pylab as plt
+from time import time
+from lib import (clean_directory, simulate_GPe_cell,
+                 plot_data, plot_current,
+                 plot_channel_currents,
+                 make_grid)
+from input_factory import get_step_current
+from brian2.units.constants import faraday_constant as Faraday
+
+# if not os.path.exists("data"):
+#     os.makedirs("data")
+
+if __name__ == "__main__":
+
+    par = {
+        'num': 1,
+        'v0': -65*b2.mV,
+        'Cm': 1 * b2.uF,
+
+        'eNa': 50*b2.mV,
+        'eLeak': -60*b2.mV,
+        'eK': -90.*b2.mV,
+        'eCa': 130 * b2.mV,
+        'eCat': -30*b2.mV,
+
+        'gnafbar': 50*b2.mS,
+        'gnapbar': 0.1*b2.mS,
+        'gkv2bar': 0.1*b2.mS,
+        'gkv3bar': 10*b2.mS,
+        'gleak': 0.068*b2.mS,
+        'gkv4fbar': 2.*b2.mS,
+        'gkv4sbar': 1.*b2.mS,
+        'gkcnqbar': 0.2*b2.mS,
+        'ghcnbar': 0.1*b2.mS,
+        'gcahbar': 0.3*b2.mS,
+        'gskbar': 0.4*b2.mS,
+        # 'faraday_const': Faraday,
+
+        # 'Gc_Ca_sat': 5.0,
+        'Gcan50': (0.35*b2.mM)**4.6,
+    }
+
+    par_sim = {
+        'integration_method': "rk2",
+        'simulation_time': 200 * b2.ms,  # 2500 * b2.ms,
+        'dt': 0.01 * b2.ms,
+    }
+
+    # Figure 1e -----------------------------------------------------
+
+    def plot_details(filename='figure_1.png'):
+        par['record_from'] = ["vg", "Iapp",
+                              "iNap", "iNaf",
+                              'iKv2', 'iKv3',
+                              'iKv4f', 'iKv4s',
+                              'iCah', 'iSk',
+                              'iKcnq']
+        par_sim['simulation_time'] = 100 * b2.ms
+        current_unit = b2.uA
+        start_time = time()
+        i_stim = [2.0]
+        _, ax = plt.subplots(len(i_stim)+1, figsize=(6, 8), sharex=True)
+        # make_grid(nrows, ncols, left, right, hspace, wspace, bottom, top)
+        ax = make_grid(3, 1, 0.2, 0.95, 0.18, 0, 0.1, 0.95)
+        for ii in range(len(i_stim)):
+            input_current = get_step_current(50,
+                                             100,
+                                             b2.ms,
+                                             i_stim[ii] * current_unit)
+            par['iapp'] = input_current
+            st_mon = simulate_GPe_cell(par, par_sim)
+            plot_data(st_mon, ax[ii], lw=1, c='k')
+            # plot_current(st_mon, ax[-2], current_unit)
+            plot_channel_currents(st_mon, ax[1:], current_unit)
+        
+        ax[0].set_xlim(75, 90)
+        ax[1].set_xlim(75, 90)
+        ax[2].set_xlim(75, 90)
+        print("Done in {:.3f}".format(time() - start_time))
+        plt.savefig(filename)
+        plt.close()
+
+    def plot(filename="figure_0.png"):
+        par['record_from'] = ["vg", "Iapp"]
+        par_sim['simulation_time'] = 500.0 * b2.ms
+        current_unit = b2.uA
+        start_time = time()
+        i_stim = [2.0]
+        _, ax = plt.subplots(len(i_stim)+1, figsize=(10, 5), sharex=True)
+        for ii in range(len(i_stim)):
+            input_current = get_step_current(100,
+                                             300,
+                                             b2.ms,
+                                             i_stim[ii] * current_unit)
+            par['iapp'] = input_current
+            st_mon = simulate_GPe_cell(par, par_sim)
+            plot_data(st_mon, ax[ii], lw=1, c='k')
+            plot_current(st_mon, ax[-1], current_unit)
+            # plot_channel_currents(st_mon, ax[-1], current_unit)
+        ax[0].set_xlim(0, 500)
+        print("Done in {:.3f}".format(time() - start_time))
+        plt.tight_layout()
+        plt.savefig(filename)
+        plt.close()
+    
+    # plot("figure_0.png")
+    plot_details('figure_1.png')
+    clean_directory()
+
+
+# input_current = b2.TimedArray([0, i_stim[ii],
+#                                0]*b2.uA,
+#                               dt=40*b2.ms)
+# par['iapp'] = i_stim[ii]*current_unit
