e393cd61f267cae8f5ab21d05440682c63dc0688
Ziaeemehr <a.ziaeemehr@gmail.com>
2021-04-10 09:21:25 +0430

    .

--- Blenkinsop2017/run.py
+++ Blenkinsop2017/run.py
@@ -7,6 +7,8 @@ from time import time
 if not os.path.exists('data/'):
     os.makedirs('data/')
 
+np.random.seed(2)
+
 
 def set_history(hist, nstart, maxdelay):
 
@@ -18,15 +20,24 @@ def set_history(hist, nstart, maxdelay):
         y[i, :(nstart+1)] = hist[i]
 
 
-def rungeKutta_integrator(h):
+def euler_integrator(f, h):
+
+    k0 = np.zeros(N)
+    for i in range(nstart, nstart + n_iteration):
+        dy = h * f(t[i], k0, i)
+        y[:, i+1] = y[:, i] + dy
+        t[i+1] = (i - nstart + 1) * dt
+
+
+def rungeKutta_integrator(f, h):
 
     for i in range(nstart, nstart + n_iteration):
 
         k0 = np.zeros(N)
-        k1 = h * sys_eqns(t[i], k0, i)
-        k2 = h * sys_eqns(t[i] + 0.5 * h, 0.5 * k1, i)
-        k3 = h * sys_eqns(t[i] + 0.5 * h, 0.5 * k2, i)
-        k4 = h * sys_eqns(t[i] + h, k3, i)
+        k1 = h * f(t[i], k0, i)
+        k2 = h * f(t[i] + 0.5 * h, 0.5 * k1, i)
+        k3 = h * f(t[i] + 0.5 * h, 0.5 * k2, i)
+        k4 = h * f(t[i] + h, k3, i)
 
         y[:, i + 1] = y[:, i] + (1.0 / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4)
         t[i + 1] = (i - nstart + 1) * dt
@@ -36,7 +47,7 @@ def rungeKutta_integrator(h):
 
 def gompertz(x, M, B):
     inv_M = 1.0 / M
-    return (M*(inv_M*B)**(np.exp(-inv_M*math.e*y)))
+    return (M*(inv_M*B)**(np.exp(-inv_M*math.e*x)))
 
 
 def sys_eqns(t, k, n):
@@ -56,42 +67,46 @@ def sys_eqns(t, k, n):
     dPmc  : 11    dSmc : 23
     '''
 
-    yPs1 = y[0, n]
-    dyPs1 = y[1, n]
-    yPs2 = y[2, n]
-    dyPs2 = y[3, n]
-    yPstn = y[4, n]
-    dyPstn = y[5, n]
-    yPge = y[6, n]
-    dyPge = y[7, n]
-    yPgi = y[8, n]
-    dyPgi = y[9, n]
-    yPmc = y[10, n]
-    dyPmc = y[11, n]
-    ySs1 = y[12, n]
-    dySs1 = y[13, n]
-    ySs2 = y[14, n]
-    dySs2 = y[15, n]
-    ySstn = y[16, n]
-    dySstn = y[17, n]
-    ySge = y[18, n]
-    dySge = y[19, n]
-    ySgi = y[20, n]
-    dySgi = y[21, n]
-    ySmc = y[22, n]
-    dySmc = y[23, n]
+    yPs1 = y[0, n] + k[0]
+    dyPs1 = y[1, n] + k[1]
+    yPs2 = y[2, n] + k[2]
+    dyPs2 = y[3, n] + k[3]
+    yPstn = y[4, n] + k[4]
+    dyPstn = y[5, n] + k[5]
+    yPge = y[6, n] + k[6]
+    dyPge = y[7, n] + k[7]
+    yPgi = y[8, n] + k[8]
+    dyPgi = y[9, n] + k[9]
+    yPmc = y[10, n] + k[10]
+    dyPmc = y[11, n] + k[11]
+    ySs1 = y[12, n] + k[12]
+    dySs1 = y[13, n] + k[13]
+    ySs2 = y[14, n] + k[14]
+    dySs2 = y[15, n] + k[15]
+    ySstn = y[16, n] + k[16]
+    dySstn = y[17, n] + k[17]
+    ySge = y[18, n] + k[18]
+    dySge = y[19, n] + k[19]
+    ySgi = y[20, n] + k[20]
+    dySgi = y[21, n] + k[21]
+    ySmc = y[22, n] + k[22]
+    dySmc = y[23, n] + k[23]
 
-    INp = 0#IN(t, gp, a, b, coeff_ab, 0, 0.3)
-    INs = 0# IN(t, gs, a, b, coeff_ab, 0, 0.3)
+    INp = IN(t, gp, a, b, coeff_ab, 0, 0.3)
+    INs = IN(t, gs, a, b, coeff_ab, 0, 0.3)
 
     # channel I
-    ddyPs1 = inv_tau2*(-W_s_s*gompertz(ySs1, M_str, B_str)
-                       + W_sc_s*(1+da)*INp
-                       + W_mc_s*(1+da)*gompertz(
-                           interp_y(t-T_ctx_stn, 10, n), M_ctx, B_ctx)  # yPmc
-                       - W_ge_s*gompertz(
-                           interp_y(t-T_gpe_stn, 18, n), M_GP, B_GP)  # ySge
-                       ) - 2*inv_tau*dyPs1-inv_tau2*yPs1
+    ddyPs1 = inv_tau2 * (- W_s_s*gompertz(ySs1, M_str, B_str)
+                         + W_sc_s * (1 + da) * INp
+                         + W_mc_s * (1 + da) * gompertz(
+        interp_y(t-T_ctx_stn, 10, n), M_ctx, B_ctx)  # yPmc
+        - W_ge_s*gompertz(
+        interp_y(t-T_gpe_stn, 18, n), M_GP, B_GP)  # ySge
+    ) - 2*inv_tau*dyPs1-inv_tau2*yPs1
+
+    # print(gompertz(interp_y(t-T_gpe_stn, 18, n), M_GP, B_GP))
+    # print(interp_y(t-T_gpe_stn, 18, n))
+    # exit(0)
 
     ddyPs2 = inv_tau2*(-W_s_s*gompertz(ySs2, M_str, B_str)
                        + W_sc_s*(1-da)*INp
@@ -177,23 +192,23 @@ def sys_eqns(t, k, n):
         + W_sc_mc * INs
     ) - 2*inv_tau * dySmc - inv_tau2 * ySmc
 
-    return np.asarray([yPs1, dyPs1,
-                       yPs2, dyPs2,
-                       yPstn, dyPstn,
-                       yPge, dyPge,
-                       yPgi, dyPgi,
-                       yPmc, dyPmc,
-                       ySs1, dySs1,
-                       ySs2, dySs2,
-                       ySstn, dySstn,
-                       ySge, dySge,
-                       ySgi, dySgi,
-                       ySmc, dySmc
+    return np.asarray([dyPs1, ddyPs1,
+                       dyPs2, ddyPs2,
+                       dyPstn, ddyPstn,
+                       dyPge, ddyPge,
+                       dyPgi, ddyPgi,
+                       dyPmc, ddyPmc,
+                       dySs1, ddySs1,
+                       dySs2, ddySs2,
+                       dySstn, ddySstn,
+                       dySge, ddySge,
+                       dySgi, ddySgi,
+                       dySmc, ddySmc
                        ])
 
 
 def IN(t, g, a, b, coeff, t_start, t_end):
-    if (t >= t_start) and (t<=t_end):
+    if (t >= t_start) and (t <= t_end):
         return B_ctx + g * coeff * (np.exp(-b*t) - np.exp(-a*t))
     else:
         return B_ctx
@@ -212,16 +227,16 @@ if __name__ == "__main__":
     n_nucleoli = len(nucleoli)
     N = 24
     dt = 0.1
-    t_sim = 10
+    t_sim = 150
     y0 = np.random.rand(N)*10
     # delays = np.zeros((n_nucleoli, n_nucleoli), dtype=float)
-    delays = np.array([
-        [0.0, 0.0, 0.0, 0.0, 2.5],
-        [0.0, 0.0, 1.0, 0.0, 2.5],
-        [7.0, 2.5, 1.0, 0.0, 0.0],
-        [12., 2.5, 1.0, 0.0, 0.0],
-        [0.0, 0.0, 0.0, 3.0, 0.0]
-    ])
+    # delays = np.array([
+    #     [0.0, 0.0, 0.0, 0.0, 2.5],
+    #     [0.0, 0.0, 1.0, 0.0, 2.5],
+    #     [7.0, 2.5, 1.0, 0.0, 0.0],
+    #     [12., 2.5, 1.0, 0.0, 0.0],
+    #     [0.0, 0.0, 0.0, 3.0, 0.0]
+    # ])
 
     # parameters
     tau = 2.
@@ -239,40 +254,39 @@ if __name__ == "__main__":
     gp = 0.25
     gs = 0.17
 
+    W_mc_stn = 20.
+    W_ge_stn = 3.
+    W_s2_ge = 40.
+    W_stn_ge = 0.72
+    W_ge_ge = 1.37
+    W_ge_gi = 0.8
+    W_s1_gi = 4.
+    W_stn_gi = 0.2
+    W_s_s = 0.3
+    W_gi_mc = 0.25
+    W_sc_s = 4.
+    W_sc_stn = 20.
+    W_mc_s = 0.65
+    W_sc_mc = 1.
+    W_ge_s = 0.1
+    W_ge_R = 0.3
 
-    # W_mc_stn = 20.
-    # W_ge_stn = 3.
-    # W_s2_ge = 40.
-    # W_stn_ge = 0.72
-    # W_ge_ge = 1.37
-    # W_ge_gi = 0.8
-    # W_s1_gi = 4.
-    # W_stn_gi = 0.2
-    # W_s_s = 0.3
-    # W_gi_mc = 0.25
-    # W_sc_s = 4.
-    # W_sc_stn = 20.
-    # W_mc_s = 0.65
-    # W_sc_mc = 1.
-    # W_ge_s = 0.1
-    # W_ge_R = 0.3
-
-    W_mc_stn = 0.
-    W_ge_stn = 0.
-    W_s2_ge = 0.
-    W_stn_ge = 0.
-    W_ge_ge = 0
-    W_ge_gi = 0.
-    W_s1_gi = .0
-    W_stn_gi = 0.
-    W_s_s = 0.
-    W_gi_mc = 0.
-    W_sc_s = .0
-    W_sc_stn = 0.
-    W_mc_s = 0.
-    W_sc_mc = .0
-    W_ge_s = 0.
-    W_ge_R = 0.
+    # W_mc_stn = 0.
+    # W_ge_stn = 0.
+    # W_s2_ge = 0.
+    # W_stn_ge = 0.
+    # W_ge_ge = 0
+    # W_ge_gi = 0.
+    # W_s1_gi = .0
+    # W_stn_gi = 0.
+    # W_s_s = 0.
+    # W_gi_mc = 0.
+    # W_sc_s = .0
+    # W_sc_stn = 0.
+    # W_mc_s = 0.
+    # W_sc_mc = .0
+    # W_ge_s = 0.
+    # W_ge_R = 0.
 
     T_ctx_str = 2.5
     T_ctx_stn = 2.5
@@ -289,25 +303,24 @@ if __name__ == "__main__":
     inv_tau = 1 / tau
     inv_tau2 = inv_tau * inv_tau
     coeff_ab = a*b/(a-b)
-    maxdelay = np.max(delays)
+    maxdelay = 12.  # np.max(delays)
     nstart = 50
     n_iteration = (int)((t_sim)/dt)
 
     y = np.zeros((N, n_iteration+nstart+1))
     t = np.zeros(n_iteration+nstart+1)
     set_history(y0, nstart, maxdelay)
-    rungeKutta_integrator(dt)
-
-    
+    # rungeKutta_integrator(sys_eqns, dt)
+    euler_integrator(sys_eqns, dt)
 
     print("Done in %g seconds" % (time()-start))
     labels = ['str1', 'str2', 'stn', 'gpe', 'gpi']
-    for i in range(0,10,2):
-        plt.plot(t, y[i, :], label=labels[i//2])
+    for i in range(0, 10, 2):
+        plt.plot(t, y[i, :]/1000, label=labels[i//2])
 
     plt.legend()
     plt.savefig(f"data/{output_filename}.png", dpi=150)
-
+    plt.show()
 
     # y = np.arange(0, 100, 1)
     # f = gompertz(y, M_GP, B_GP)
